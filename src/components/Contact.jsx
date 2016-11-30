3147d573dfe268074aa0fbf3a921b701376c441b
emyarod <allllaboutyou@gmail.com>
2016-11-30 01:46:16 -0800

    Add Contact component

---
+++ src/components/Contact.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+export default function Contact() {
+  return (
+    <div className="contact">
+      <h1>Contact</h1>
+      <div className="jumbotron">
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+      </div>
+      <div className="block contact-form">
+        <div className="wrap">
+          <h2>Contact form</h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        </div>
+      </div>
+    </div>
+  );
+}
