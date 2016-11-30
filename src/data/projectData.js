3c0417c5696e09187618bdb070527865eb88b15d
emyarod <allllaboutyou@gmail.com>
2016-11-30 01:44:14 -0800

    Add project data layer Temporarily fill with fake data

---
+++ src/data/projectData.js
@@ -0,0 +1,89 @@
+import Project1 from '../components/projects/Project-1';
+import Project2 from '../components/projects/Project-2';
+import Project3 from '../components/projects/Project-3';
+
+const projectData = [
+  {
+    name: 'Project 1',
+    path: 'project-1',
+    jumbotronContent: {
+      img: 'jumbotronImg',
+      blurb: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+    },
+    summary: {
+      type: 'Web Application',
+      role: 'Design & Development',
+      tools: [
+        'Tool 1',
+        'Tool 2',
+        'Tool 3',
+        'Tool 4',
+        'Tech 1',
+        'Tech 2',
+        'Tech 3',
+        'Tech 4',
+      ],
+      projectLink: {
+        text: 'text',
+        href: 'href',
+      },
+    },
+    details: Project1,
+  },
+  {
+    name: 'Project 2',
+    path: 'project-2',
+    jumbotronContent: {
+      img: 'jumbotronImg',
+      blurb: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+    },
+    summary: {
+      type: 'Web Application',
+      role: 'Design & Development',
+      tools: [
+        'Tool 1',
+        'Tool 2',
+        'Tool 3',
+        'Tool 4',
+        'Tech 1',
+        'Tech 2',
+        'Tech 3',
+        'Tech 4',
+      ],
+      projectLink: {
+        text: 'text',
+        href: 'href',
+      },
+    },
+    details: Project2,
+  },
+  {
+    name: 'Project 3',
+    path: 'project-3',
+    jumbotronContent: {
+      img: 'jumbotronImg',
+      blurb: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+    },
+    summary: {
+      type: 'Web Application',
+      role: 'Design & Development',
+      tools: [
+        'Tool 1',
+        'Tool 2',
+        'Tool 3',
+        'Tool 4',
+        'Tech 1',
+        'Tech 2',
+        'Tech 3',
+        'Tech 4',
+      ],
+      projectLink: {
+        text: 'text',
+        href: 'href',
+      },
+    },
+    details: Project3,
+  },
+];
+
+export default projectData;
