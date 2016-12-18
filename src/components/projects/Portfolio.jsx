98a3172e814affe91fabcb06c88d5c303f5818ae
emyarod <allllaboutyou@gmail.com>
2016-12-17 17:22:39 -0800

    Add portfolio project component

---
+++ src/components/projects/Portfolio.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export default function Portfolio() {
+  return (
+    <div>
+      <p>
+        My personal website, built with React. The site was designed to showcase my work in a clean and quick loading interface.
+      </p>
+      <p>
+        With an emphasis on speed in mind, I chose to render the site using React, and all of the routing is handled by React Router. Since updates and changes to the DOM are only applied when necessary, navigating the site feels quick and snappy.
+      </p>
+      <p>
+        Another point of emphasis when creating the site was responsiveness. Usage of media queries and flex box allows for the site to look consistent on virtually any device screen size and screen density. All of the icons on the site are also created with SVG to maintain clarity.
+      </p>
+    </div>
+  );
+}
