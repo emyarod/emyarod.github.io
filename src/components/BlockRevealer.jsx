e3513206664688d332c3d56579e4c27ffdc8c9fa
emyarod <allllaboutyou@gmail.com>
2016-12-24 00:42:24 -0800

    Add BlockRevealer component

---
+++ src/components/BlockRevealer.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Watch } from 'scrollmonitor-react';
+
+export default Watch(props => {
+  return (
+    <div className={`block-revealer ${props.class}`}>
+      <div
+        className="block-revealer__content"
+        style={{
+          'animationDelay': props.animationDelay
+            ? `${props.animationDelay + .5}s`
+            : '.5s',
+        }}
+      >
+        {props.children}
+      </div>
+
+      <div
+        className="block-revealer__concealer"
+        style={{
+          'backgroundColor': props.bgc ? props.bgc : '#00b259',
+          'animationDelay': props.animationDelay
+            ? `${props.animationDelay}s, ${props.animationDelay + .5}s`
+            : '0s, .5s',
+        }}
+      >
+      </div>
+    </div>
+  );
+});
