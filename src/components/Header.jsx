ab5aab25cdb686e3f3d95b1445aaa7a5b09cf557
emyarod <allllaboutyou@gmail.com>
2016-12-27 01:22:01 -0800

    Inline SVG logo and make header responsive

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,22 +1,32 @@
 import React from 'react';
 import Headroom from 'react-headroom';
+import Isvg from 'react-inlinesvg';
+import MediaQuery from 'react-responsive';
 import { Link } from 'react-router';
 import logo from '../logo.svg';
 
 export default function Header() {
   return (
-    <Headroom>
-      <header className="App-header">
-        <nav>
-          <img src={logo} className="App-logo" alt="logo" />
-          <div>
-            {/* FIXME: activeStyle/activeClassName */}
-            <Link to="/">Home</Link>
-            <Link to="/portfolio" activeClassName="active">Portfolio</Link>
-            <Link to="/contact" activeClassName="active">Contact</Link>
-          </div>
-        </nav>
-      </header>
-    </Headroom>
+    <MediaQuery minWidth={576}>
+      {() => (
+        <Headroom>
+          <header className="App-header">
+            <Isvg
+              className="Wang"
+              src={logo}
+              uniquifyIDs={false}
+              alt="Home"
+            >
+            </Isvg>
+            <nav className="nav">
+              {/* FIXME: activeStyle/activeClassName */}
+              <Link to="/">Home</Link>
+              <Link to="/portfolio" activeClassName="active">Portfolio</Link>
+              <Link to="/contact" activeClassName="active">Contact</Link>
+            </nav>
+          </header>
+        </Headroom>
+      )}
+    </MediaQuery>
   );
 }
