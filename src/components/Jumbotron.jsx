4a6454d8d98594883e98a40553e15e69f39c8116
emyarod <allllaboutyou@gmail.com>
2016-12-26 19:21:18 -0800

    Simplify Jumbotron component Remove switch Use props to determine background image

--- src/components/Jumbotron.jsx
+++ src/components/Jumbotron.jsx
@@ -1,59 +1,41 @@
 import React from 'react';
-import projectData from '../data/projectData';
-import ContactImg from '../static/img/Contact/contact.jpg';
+import MediaQuery from 'react-responsive';
 
-export default function Jumbotron(props) {
-  const project = projectData.find(
-    element => `/portfolio/${element.path}` === props.pathname
-  ) || props.pathname;
+export default function Jumbotron({ bgp, pathname, landing, img, children }) {
+  const styles = {
+    backgroundSize: 'cover',
+    backgroundPosition: bgp ? bgp : 'center 20%',
+  };
 
-  switch (project) {
-    case '/':
-      return (
-        <div className="jumbotron landing">
-          <div className="wrap">
-            <h1>Andrew Wang</h1>
-            <h5 className="serif">
-              <i>Front-end web developer based in the Bay Area</i>
-            </h5>
-          </div>
+  const scrim = `
+    linear-gradient(to bottom,rgba(255,255,255,.8) 0,
+    rgba(255,255,255,.6) 100%)
+  `;
+
+  return (
+    <div className="jumbotron-container">
+      <MediaQuery minWidth={1201}>
+        <div
+          className={`jumbotron ${landing ? 'landing' : null}`}
+          style={{
+            ...styles,
+            'backgroundImage': `url(${img})`,
+          }}
+        >
+          <div className="wrap">{children}</div>
         </div>
-      );
-    case '/portfolio':
-      return (
-        <div className="jumbotron">
-          <div className="wrap">
-            <h1>Portfolio</h1>
-            <div>
-              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-            </div>
-          </div>
+      </MediaQuery>
+      <MediaQuery maxWidth={1200}>
+        <div
+          className={`jumbotron ${landing ? 'landing' : null}`}
+          style={{
+            ...styles,
+            'backgroundImage': `${scrim}, url(${img})`,
+          }}
+        >
+          <div className="wrap">{children}</div>
         </div>
-      );
-    case '/contact':
-      return (
-        <div className="jumbotron" style={{
-          'backgroundImage': `linear-gradient(to bottom,rgba(255,255,255,.8) 0,rgba(255,255,255,.6) 100%), url(${ContactImg})`,
-          'backgroundSize': 'cover',
-          'backgroundPosition': 'center 20%',
-        }}>
-          <div className="wrap">
-            <h1>Let’s have a chat.</h1>
-            <p>I'm always interested in new opportunities. If you have a project you'd like to discuss, or even if you just want to find out more about me, get in touch with me so we can get started.</p>
-          </div>
-        </div>
-      );
-    case projectData.find(e => `/portfolio/${e.path}` === props.pathname):
-      return (
-        <div className="jumbotron">
-          <div className="wrap">
-            <h1>{project.name}</h1>
-            <p>{project.jumbotronContent.img}</p>
-            <p>{project.jumbotronContent.blurb}</p>
-          </div>
-        </div>
-      );
-    default:
-      return null;
-  }
+      </MediaQuery>
+    </div>
+  );
 }
