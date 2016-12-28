d07dc2b148d3f4dcef84961d5a97fb655f8fc530
emyarod <allllaboutyou@gmail.com>
2016-12-27 17:29:06 -0800

    Destructure props

--- src/components/AnimatedSVG.jsx
+++ src/components/AnimatedSVG.jsx
@@ -3,11 +3,11 @@ import Isvg from 'react-inlinesvg';
 import { Watch } from 'scrollmonitor-react';
 import FadeIn from './FadeIn';
 
-export default Watch(props => {
+export default Watch(({ src, alt }) => {
   return (
     <div className="svg-container">
       <FadeIn>
-        <Isvg src={props.src} uniquifyIDs={false} alt={props.alt}></Isvg>
+        <Isvg src={src} uniquifyIDs={false} alt={alt}></Isvg>
       </FadeIn>
     </div>
   );
