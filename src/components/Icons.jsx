fcafd00ceb0c81dba3732e421538e74be3c4def8
emyarod <allllaboutyou@gmail.com>
2016-12-27 17:18:44 -0800

    Add class names to svg icon components

--- src/components/Icons.jsx
+++ src/components/Icons.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function Icon({ icon }) {
   return (
-    <svg viewBox="0 0 1024 1024">
+    <svg className="icon" viewBox="0 0 1024 1024">
       <path d={icon}></path>
     </svg>
   );
