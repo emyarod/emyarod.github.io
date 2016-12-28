336bc730b2e11176abd5f7c23bb3af0667d59d89
emyarod <allllaboutyou@gmail.com>
2016-12-27 17:58:43 -0800

    Add 404 content and styles

--- src/components/404.jsx
+++ src/components/404.jsx
@@ -2,9 +2,14 @@ import React from 'react';
 
 export default function FourOhFour() {
   return (
-    <div className="block dark">
+    <div className="block fof">
       <div className="wrap">
-        <h1>404 not found.</h1>
+        <div className="content">
+          <h1 className="text-center">404</h1>
+          <p className="text-center">
+            The page you are looking for can't be found.
+          </p>
+        </div>
       </div>
     </div>
   );
