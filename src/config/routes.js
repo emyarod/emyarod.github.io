dd1e85db7150bba50eb427d7f5606bf107f6bc2b
emyarod <allllaboutyou@gmail.com>
2016-12-31 00:29:53 -0800

    Remove TODOs

--- src/config/routes.js
+++ src/config/routes.js
@@ -2,10 +2,7 @@ import Home from '../components/Home';
 import Portfolio from '../components/Portfolio';
 import Contact from '../components/Contact';
 
-// FIXME: no longer true vvvvvvvvvvvv
-// Each "route" has two components, one for the sidebar
-// and one for the main area, we'll render them
-// simultaneously when the pattern matches
+// component is rendered when the pattern matches
 const routes = [
   {
     main: Home,
