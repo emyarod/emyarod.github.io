a4f59a6546305143d5baa6ed4e7310bafe10f72f
emyarod <allllaboutyou@gmail.com>
2016-12-30 19:47:00 -0800

    Remove commented code

--- src/App.js
+++ src/App.js
@@ -7,7 +7,6 @@ import routes from './config/routes';
 import FourOhFour from './components/404';
 
 const App = () => (
-  // <BrowserRouter history={history}>
   <BrowserRouter>
     <div>
       <Header />
@@ -18,11 +17,8 @@ const App = () => (
           <Match
             key={route.pattern}
             pattern={route.pattern}
-            // component={route.main}
             render={matchProps => {
               const Component = route.main;
-
-              // TODO: double check where to put scrolling
               window.scroll(0, 0);
 
               return (
