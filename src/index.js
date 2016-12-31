f08fa9edef3e8fc31b9628779b29794e0785cb96
emyarod <allllaboutyou@gmail.com>
2016-12-30 19:47:36 -0800

    Change page title with window visibility

--- src/index.js
+++ src/index.js
@@ -3,6 +3,13 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './index.scss';
 
+// change page title with window visibility
+document.addEventListener('visibilitychange', () => (
+  document.hidden
+    ? document.title = document.title.replace(' - ', ' | ')
+    : document.title = document.title.replace(' | ', ' - ')
+));
+
 ReactDOM.render(
   <App />,
   document.getElementById('root')
