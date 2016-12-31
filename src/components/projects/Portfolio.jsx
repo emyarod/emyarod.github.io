49278322624d87367762a1adb3fdab470d9904db
emyarod <allllaboutyou@gmail.com>
2016-12-31 00:31:15 -0800

    Update portfolio project details

--- src/components/projects/Portfolio.jsx
+++ src/components/projects/Portfolio.jsx
@@ -3,11 +3,17 @@ import React from 'react';
 export default function Portfolio() {
   return (
     <div>
+      <h4 className="label">Project overview</h4>
       <p>
         My personal website, built with React. The site was designed to showcase my work in a clean and quick loading interface.
       </p>
+      <br />
+      <h4 className="label">The Process</h4>
       <p>
         With an emphasis on speed in mind, I chose to render the site using React, and all of the routing is handled by React Router. Since updates and changes to the DOM are only applied when necessary, navigating the site feels quick and snappy.
+      </p>
+      <p>
+        The site contains five main pages: the home page, a portfolio page, a contact page, a project page template, and a 404 page. Since every page contains the header and footer, I decided to make each page its own component. Because of this, the header and footer are not re-rendered while navigating through the website.
       </p>
       <p>
         Another point of emphasis when creating the site was responsiveness. Usage of media queries and flex box allows for the site to look consistent on virtually any device screen size and screen density. All of the icons on the site are also created with SVG to maintain clarity.
