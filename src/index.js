8e8caa4acc58444eb11805460308309808ad5ed0
emyarod <afw5059@gmail.com>
2019-08-18 20:09:35 -0500

    add service worker

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.scss';
+import * as serviceWorker from './serviceWorker';
 
 // change page title with window visibility
 document.addEventListener('visibilitychange', () => (
@@ -38,7 +39,9 @@ console.info(`
 Designed and developed by Andrew Wang.
   `);
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+ReactDOM.render(<App />, document.getElementById('root'));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
